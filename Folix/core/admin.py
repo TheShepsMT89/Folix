8a2db1769b22d8bf77e3cb4cd60eac2d15e75b29
TheShepsMT <theshepsmt@gmail.com>
2023-12-01 21:41:58 -0500

    Comenzando con los modelos

--- Folix/core/admin.py
+++ Folix/core/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
 # Register your models here.
+admin.site.site_header = "Administración de Folix"
+admin.site.site_title = "Administración Folix"
--- Folix/core/static/core/js/script.js
+++ Folix/core/static/core/js/script.js
@@ -5,13 +5,13 @@ const btnLogin = document.getElementById("boton");
 const btnRegistro = document.getElementById("registro");
 const btnLogin2 = document.getElementById("boton2");
 
-function AparecerLogin(login) {
+function AparecerLogin() {
   login.classList.add("scale-up-center");
   login.style.display = "block";
   registro.style.display = "none";
 }
 
-function AparecerRegistro(registro) {
+function AparecerRegistro() {
   registro.classList.add("scale-up-center");
   login.style.display = "none";
   registro.style.display = "block";
@@ -22,6 +22,13 @@ function DesaparecerBienvenida() {
   textoPrincipal.classList.add("scale-down-blur-center");
   setTimeout(function () {
     bienvenida.style.display = "none";
+  }, 360);
+}
+
+function DesaparecerRegistro() {
+  registro.classList.add("scale-down-blur-center");
+  setTimeout(function () {
+    registro.style.display = "none";
   }, 360);
 }
 
